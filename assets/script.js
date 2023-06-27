808771d86cd92b1cd98e922a6c0e31aa19926286
ThomasHascat <dutourne.t@gmail.com>
2023-06-27 10:35:52 +0200

    :add dot_selected

--- assets/script.js
+++ assets/script.js
@@ -1,19 +1,19 @@
 const slides = [
 	{
-		"image":"slide1.jpg",
-		"tagLine":"Impressions tous formats <span>en boutique et en ligne</span>"
+		"image": "slide1.jpg",
+		"tagLine": "Impressions tous formats <span>en boutique et en ligne</span>"
 	},
 	{
-		"image":"slide2.jpg",
-		"tagLine":"Tirages haute définition grand format <span>pour vos bureaux et events</span>"
+		"image": "slide2.jpg",
+		"tagLine": "Tirages haute définition grand format <span>pour vos bureaux et events</span>"
 	},
 	{
-		"image":"slide3.jpg",
-		"tagLine":"Grand choix de couleurs <span>de CMJN aux pantones</span>"
+		"image": "slide3.jpg",
+		"tagLine": "Grand choix de couleurs <span>de CMJN aux pantones</span>"
 	},
 	{
-		"image":"slide4.png",
-		"tagLine":"Autocollants <span>avec découpe laser sur mesure</span>"
+		"image": "slide4.png",
+		"tagLine": "Autocollants <span>avec découpe laser sur mesure</span>"
 	}
 ];
 
@@ -22,23 +22,27 @@ const slides = [
 var leftArrow = document.querySelector(".arrow_left");
 var rightArrow = document.querySelector(".arrow_right");
 var dots = document.querySelector(".dots");
-var slide= document.querySelector(".banner-img");
+var slide = document.querySelector(".banner-img");
 
 
 
 
 //var dot = document.createElement(".dot");
 
-for(var i = 0; i< slides.length; i++){
-	var div= document.createElement("div");
+for (var i = 0; i < slides.length; i++) {
+	var div = document.createElement("div");
 
 	div.textContent = slides[i];
 
-
 	div.classList.add('dot');
 
+	if (i === 0) {
+		div.classList.add('dot_selected');
+	  }
 
 	dots.appendChild(div);
+
+
 }
 
 
@@ -48,13 +52,13 @@ for(var i = 0; i< slides.length; i++){
 
 
 //Listener
-leftArrow.addEventListener('click',previous);
-rightArrow.addEventListener('click',next);
+leftArrow.addEventListener('click', previous);
+rightArrow.addEventListener('click', next);
 
-function next(){
+function next() {
 	console.log('right');
 };
-function previous(){
+function previous() {
 	console.log('left');
 };
 
@@ -64,9 +68,9 @@ function previous(){
 
 
 
-	
 
-	
+
+
 
 
 
@@ -75,4 +79,4 @@ function previous(){
 //Compte le nombre d'élement de SLIDES
 for (var i = 0; i < slides.length; i++) {
 	console.log(slides[i]);
-  }
+}
