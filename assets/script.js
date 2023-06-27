9582f9f40054c75727028b6f8cf1d22ff503b772
ThomasHascat <dutourne.t@gmail.com>
2023-06-27 11:57:10 +0200

    :add changeSlide

--- assets/script.js
+++ assets/script.js
@@ -22,27 +22,27 @@ const slides = [
 var leftArrow = document.querySelector(".arrow_left");
 var rightArrow = document.querySelector(".arrow_right");
 var dots = document.querySelector(".dots");
-var slide = document.querySelector(".banner-img");
-
+var slideImage= document.querySelector(".banner-img");
+var indexCurrent = 0;
 
 
 
 //var dot = document.createElement(".dot");
 
 for (var i = 0; i < slides.length; i++) {
+
+	//CREER UNE DIV POUR CHAQUE ELEMENT DU TABLEAU
 	var div = document.createElement("div");
 
 	div.textContent = slides[i];
-
+	//AJOUT LA CLASS ("dot")
 	div.classList.add('dot');
-
+	//SI ACTIVE ALORS AJOUTE CLASS("DOT_SELECTED")
 	if (i === 0) {
 		div.classList.add('dot_selected');
-	  }
+	}
 
 	dots.appendChild(div);
-
-
 }
 
 
@@ -51,9 +51,39 @@ for (var i = 0; i < slides.length; i++) {
 
 
 
+
+
 //Listener
-leftArrow.addEventListener('click', previous);
-rightArrow.addEventListener('click', next);
+leftArrow.addEventListener('click', function () {
+	indexCurrent = (indexCurrent === 0) ? slides.length - 1 : indexCurrent - 1;
+	updateSlide();
+	
+
+});
+
+rightArrow.addEventListener('click', function () {
+	indexCurrent= (indexCurrent === slides.length - 1) ? 0 : indexCurrent + 1;
+	updateSlide();
+	
+})
+
+function changeSlide(next) {
+	if (next) {
+	  indexCurrent = (indexCurrent === slides.length - 1) ? 0 : indexCurrent + 1;
+	} else {
+	  indexCurrent = (indexCurrent === 0) ? slides.length - 1 : indexCurrent - 1;
+	}
+	updateSlide();
+  }
+function updateSlide() {
+	slideImage.src = "assets/images/slideshow/" + slides[indexCurrent].image;
+	var dots= document.querySelectorAll('.dots .dot');
+
+	dots.forEach(function (dot,index){
+		dot.classList.toggle('dot_selected', index ===indexCurrent)
+	});
+	
+}
 
 function next() {
 	console.log('right');
